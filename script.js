f9250586b36fb0a874722a1f697975824d3bd467
mskashef <mohammadsadeghmk@gmail.com>
2021-11-06 22:11:02 +0330

    v1

---
+++ script.js
@@ -0,0 +1,586 @@
+const cv = {
+  summary: `I'm MSK, And I'm a Front End Developer. I'm Flexible against learning new concepts.
+    In 2017, I chose my way, which was being a Front End developer. Then I started my self-learning process. I just played a lot with HTML, CSS and after a year, I started doing my first projects.
+    In July 2019, I started working with React.js. Always I was trying to expand my knowledge and I love to learn new concepts.
+    I also have interests and talents in some fields such as painting, music, working with Electronics, etc.`,
+  name: 'M.S Kashef',
+  contactInfo: [
+    {
+      icon: 'assets/icons/email.svg',
+      title: 'Email',
+      value: 'mohammadsadeghmk[at]gmail.com',
+    },
+    {
+      icon: 'assets/icons/phone-number.svg',
+      title: 'Phone number',
+      value: '+98 901 061 5710',
+    },
+    {
+      icon: 'assets/icons/location.svg',
+      title: 'Location',
+      value: 'Iran, West Azerbaijan, Urmia',
+    },
+    {
+      icon: 'assets/icons/instagram.svg',
+      title: 'Instagram',
+      value: 'MSK.CODES',
+    },
+    {
+      icon: 'assets/icons/linkedin.svg',
+      title: 'Linkedin',
+      value: 'mskashef',
+    },
+    {
+      icon: 'assets/icons/github.svg',
+      title: 'GitHub',
+      value: 'mskashef',
+    },
+  ],
+  skills: [
+    {
+      title: 'Working',
+      skills: [
+        'ReactJs',
+        'JavaScript',
+        'HTML',
+        'CSS',
+        'JQuery',
+        'TypeScript',
+        'Webpack',
+        'NodeJs',
+        'ExpressJs',
+        'Java',
+        'Git',
+      ],
+    },
+    {
+      title: 'Basic',
+      skills: ['C', 'PHP', 'JavaFx'],
+    },
+    {
+      title: 'Softwares',
+      skills: ['Adobe XD'],
+    },
+    {
+      title: 'Soft Skills',
+      skills: [
+        'Responsibility',
+        'Entrepreneurship',
+        'IoEmotional IntelligenceT',
+        'Decision-making power',
+        'Teaching',
+        'Criticism of',
+        'Creativity',
+        'Problem-solving skills',
+      ],
+    },
+  ],
+  experiences: {
+    industrial: [
+      {
+        title:
+          'React Developer at <a target="_blank" href="https://liboberry.com/">LiboBerry</a>',
+        companyName: 'LiboBerry',
+        icon: 'assets/img/industrial/liboberry.png',
+        date: 'May 2021 - Present',
+        description:
+          'LiboBerry is an AI-Powered scientific writing assistant service.',
+        tasks: [
+          `Developed a custom WYSIWYG Editor with special facilities like suggestion support based on the AI model's output.`,
+          `Developed a WebApp for user dashboard with the possibility of Project Creation, Edit, Upload the reference articles, ... .`,
+          `Developed a WebApp which use can enter data and get the standart HTML/XML output.`,
+        ],
+        tags: [
+          'ReactJs',
+          'React Hooks',
+          'JS',
+          'HTML',
+          'CSS',
+          'JSX',
+          'Adobe XD',
+        ],
+      },
+      {
+        title:
+          'React Developer at <a target="_blank" href="https://www.learnpersianart.com/">Rhythmitica</a>',
+        companyName: 'Rhythmitica',
+        icon: 'assets/img/industrial/mashgh-eshgh.png',
+        date: 'Jun 2020 - Dec 2020',
+        description:
+          'Rhythmitica is an online musical instrument tutoring academy',
+        tasks: [
+          `Developed an online shop for music instruments.`,
+          `Developed a WebApp for class reservation.`,
+          `Developed a PWA for class management.`,
+          `Developed a custom calendar component for managing classes.`,
+          `Designed a UI for the online shop using.`,
+          `Designed a UI for the class reservation website using.`,
+          `Designed a UI for the PWA.`,
+        ],
+        tags: [
+          'ReactJs',
+          'React Hooks',
+          'JS',
+          'HTML',
+          'SASS',
+          'JSX',
+          'Adobe XD',
+        ],
+      },
+      {
+        title:
+          'Frontend Developer at <a target="_blank" href="https://ex-changelly.com/">Changelly</a>',
+        companyName: 'Changelly',
+        icon: 'assets/img/industrial/changelly.png',
+        date: 'May 2020 - Jul 2020',
+        description: 'Changelly is an online cryptocurrency shop.',
+        tasks: [
+          `Developed a HomePage.`,
+          `Developed a custom chart library with SVG & JS.`,
+        ],
+        tags: ['JS', 'SVG', 'HTML', 'CSS'],
+      },
+      {
+        title:
+          'UI Designer at <a target="_blank" href="https://www.farazbal.com/">Farazbal</a>',
+        companyName: 'Farazbal',
+        icon: 'assets/img/industrial/farazbal.png',
+        date: 'May 2019',
+        description:
+          'Farazbal is an online website which introduces the products.',
+        tasks: [
+          `Developed some components of the website.`,
+          `Designed a UI for the website.`,
+        ],
+        tags: ['JS', 'HTML', 'CSS', 'Adobe XD'],
+      },
+      {
+        title: 'Web Developer and UI Designer at Satell',
+        companyName: 'Satell',
+        icon: 'assets/img/industrial/satell.png',
+        date: 'Jul 2019 - Apr 2020',
+        description:
+          'Satell is an online sales platform for connecting local shops and customers.',
+        tasks: [
+          `Developed a landing page.`,
+          `Designed a UI for the website.`,
+          `Designed Satell's Logo`,
+        ],
+        tags: ['JS', 'HTML', 'CSS', 'Adobe XD', 'JQuery'],
+      },
+      {
+        title: 'React Developer at 30Bime',
+        companyName: '30Bime',
+        icon: 'assets/img/industrial/1405.png',
+        date: 'July 2019 - May 2020',
+        description: '30Bime is an online insurance panel.',
+        tasks: [
+          `Designed and Developed an insurance panel with ReactJs.`,
+          `Designed and Developed a Login Page with HTML, CSS, JS.`,
+        ],
+        tags: ['React', 'Redux', 'JS', 'HTML', 'CSS', 'Adobe XD', 'JQuery'],
+      },
+    ],
+    academic: [
+      {
+        title: 'Teaching Assistant, Internet Engineering',
+        icon: 'assets/img/academic/uu.svg',
+        date: 'Fall 2021',
+        description:
+          'Faculty of electrical and computer engineering, Urmia University, Urmia, Iran',
+      },
+      {
+        title: 'Teaching Assistant, Java Advanced Programming',
+        icon: 'assets/img/academic/uu.svg',
+        date: 'Fall 2020',
+        description:
+          'Faculty of electrical and computer engineering, Urmia University, Urmia, Iran',
+      },
+    ],
+  },
+  certificates: [
+    {
+      title: 'Project-Oriented course In Front-end Web Development',
+      date: 'Oct 2020',
+      icon: 'assets/img/certificates/frontend.png',
+      description: 'Quera College Website',
+      link: `Link: <a href="https://quera.ir/certificate/wXHShP8t/">https://quera.ir/certificate/wXHShP8t/</a>`,
+    },
+    {
+      title: 'Fundamentals of java programing and Algorithmic thinking',
+      date: 'Jan 2019',
+      icon: 'assets/img/certificates/java.png',
+      description: 'Quera College Website',
+      link: `Link: <a href="https://quera.ir/certificate/XFzqy6tx/">https://quera.ir/certificate/XFzqy6tx/</a>`,
+    },
+    {
+      title: 'Attendance 54 hours of Urmia Lake startup weekend',
+      date: 'Nov 2018',
+      icon: 'assets/img/certificates/startup-weekend.png',
+      description: 'Urmia Lake National Festival',
+      link: ``,
+    },
+  ],
+  languages: [
+    {
+      title: 'Azərbaycan dili',
+      amount: 100,
+    },
+    {
+      title: 'Persian',
+      amount: 100,
+    },
+    {
+      title: 'English',
+      amount: 50,
+    },
+  ],
+  opensourceProjects: [
+    {
+      title: 'React Simple Piano',
+      tags: ['React', 'JavaScript', 'HTML5', 'CSS3'],
+      icon: 'assets/img/opensource-projects/react-piano.png',
+      date: 'Sep 2020',
+      description: `This is a simple opensource piano for someone who want's to learn a bit about working with AudioContext in
+      js an also It's a good practice to increase your skills in web development.`,
+      link: 'Link: <a href="https://github.com/mskashef/react-piano" target="_blank">https://github.com/mskashef/react-piano</a>',
+    },
+    {
+      title: 'React Ripple Effect',
+      tags: ['React', 'JavaScript', 'HTML5', 'CSS3'],
+      icon: 'assets/img/opensource-projects/react-ripple-effect.svg',
+      date: 'Sep 2020',
+      description: `In this project I have implemented the Ripple Effect with using no libraries.`,
+      link: 'Link: <a href="https://github.com/mskashef/react-ripple-effect" target="_blank">https://github.com/mskashef/react-ripple-effect</a>',
+    },
+    {
+      title: 'PhotoZone',
+      tags: ['React', 'JavaScript', 'HTML5', 'CSS3'],
+      icon: 'assets/img/opensource-projects/photozone.svg',
+      date: 'Jul 2020',
+      description: `A social media for sharing images (Like Pinterest and Instagram) [university project]`,
+      link: 'Link: <a href="https://github.com/mskashef/photozone-frontend" target="_blank">https://github.com/mskashef/photozone-frontend</a>',
+    },
+    {
+      title: 'PhotoZone',
+      tags: ['React', 'JavaScript', 'HTML5', 'CSS3'],
+      icon: 'assets/img/opensource-projects/html5-snake-game.svg',
+      date: 'Dec 2019',
+      description: `The popular Snake game written with HTML, CSS, JS`,
+      link: 'Link: <a href="https://github.com/mskashef/HTML-5-Snake-Game" target="_blank">https://github.com/mskashef/HTML-5-Snake-Game</a>',
+    },
+    {
+      title: 'HiChat',
+      tags: [
+        'React',
+        'Redux',
+        'SASS',
+        'ES6',
+        'JavaScript',
+        'HTML5',
+        'CSS3',
+        'NodeJs',
+        'MQTT',
+        'Adobe XD',
+      ],
+      icon: 'assets/img/opensource-projects/hichat.svg',
+      date: 'Dec 2019',
+      description: `A messenger web application`,
+      link: 'Link: Is being developed... (Screenshot is available in my Sample Works part in this CV.',
+    },
+    {
+      title: 'Happy Programmers Day Animation',
+      tags: ['JavaScript', 'HTML5', 'CSS3'],
+      icon: 'assets/img/opensource-projects/happy-programmers-day.png',
+      date: 'Sep 2018',
+      description: `A beautiful 3D animation using HTML, CSS, JS`,
+      link: 'Link: <a href="https://github.com/mskashef/HappyProgramersDayAnimation" target="_blank">https://github.com/mskashef/HappyProgramersDayAnimation</a>',
+    },
+  ],
+  sampleworks: [
+    {
+      title: 'Photozone',
+      src: 'assets/img/sampleworksScreenshots/photozone.png',
+      tags: ['JS', 'React', 'Adobe XD', 'SASS', 'HTML', 'JSX', 'NodeJs'],
+    },
+    {
+      title: 'ITICA PWA',
+      src: 'assets/img/sampleworksScreenshots/itica-pwa.png',
+      tags: ['JS', 'React', 'CSS', 'HTML', 'Material UI'],
+    },
+    {
+      title: 'Hooman Khalatbari',
+      src: 'assets/img/sampleworksScreenshots/hooman-khalatbari.png',
+      tags: ['JS', 'React', 'SASS', 'Adobe XD', 'HTML', 'JSX'],
+    },
+    {
+      title: 'مشق عشق',
+      src: 'assets/img/sampleworksScreenshots/mashgh-eshgh.png',
+      tags: ['JS', 'React', 'SASS', 'Adobe XD', 'HTML', 'JSX'],
+    },
+    {
+      title: 'چنجلی',
+      src: 'assets/img/sampleworksScreenshots/changelly.png',
+      tags: ['HTML', 'CSS', 'JavaScript', 'Adobe XD', 'SVG', 'Ajax', 'PHP'],
+    },
+    {
+      title: 'Rhythmitica',
+      src: 'assets/img/sampleworksScreenshots/rhythmitica.png',
+      tags: ['HTML', 'CSS', 'JavaScript', 'Adobe XD', 'JSX'],
+    },
+    {
+      title: 'Happy Programmers day Animation',
+      src: 'assets/img/sampleworksScreenshots/hpd.png',
+      tags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      title: 'HiChat',
+      src: 'assets/img/sampleworksScreenshots/hichat.png',
+      tags: ['React', 'Adobe XD', 'HTML', 'SASS', 'JSX', 'JavaScript'],
+    },
+    {
+      title: '30Bime',
+      src: 'assets/img/sampleworksScreenshots/30bime.png',
+      tags: ['React', 'Adobe XD', 'HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      title: 'Farazbal',
+      src: 'assets/img/sampleworksScreenshots/farazbal.png',
+      tags: ['Adobe XD'],
+    },
+    {
+      title: 'Satell',
+      src: 'assets/img/sampleworksScreenshots/satell.png',
+      tags: ['HTML', 'CSS', 'JS', 'JQuery', 'Adobe XD', 'Logo Design'],
+    },
+    {
+      title: 'Ahoura',
+      src: 'assets/img/sampleworksScreenshots/ahoura.png',
+      tags: ['Adobe XD'],
+    },
+  ],
+};
+
+function renderSeparators() {
+  [...$('divider')].map((divider) =>
+    $(divider).replaceWith(`<div class="sep">
+      <h4 class="sep-title">${$(divider).attr('data-title')}</h4>
+      <div class="sep-line"></div>
+    </div>`)
+  );
+}
+
+function getContacts() {
+  function getContact(title, value, icon) {
+    return `<div class="contact">
+          <img
+            class="contact-icon"
+            src="${icon}"
+            alt="${title}"
+          />
+          <span class="contact-value">${value}</span>
+        </div>`;
+  }
+  return cv.contactInfo
+    .map(({ title, value, icon }) => getContact(title, value, icon))
+    .join('\n');
+}
+
+function getSkills() {
+  function getSkill(title, skills) {
+    return `<div>
+          <b>${title}</b>
+          <ul>
+          ${skills.map((skill) => `<li>${skill}</li>`).join('\n')}
+          </ul>
+        </div>`;
+  }
+  return cv.skills
+    .map(({ title, skills }) => getSkill(title, skills))
+    .join('\n');
+}
+
+function getIndastrials() {
+  function getIndustrial(
+    title,
+    companyName,
+    icon,
+    date,
+    description,
+    tasks,
+    tags
+  ) {
+    return `<div class="card">
+          <img
+            class="industrial-card-icon"
+            src="${icon}"
+            alt="${companyName}"
+          />
+          <div>
+            <span class="card-title">${title}</span>
+            <br />
+            <b>${date}</b>
+            <div>
+            ${description}
+            </div>
+            <ul>
+            ${tasks.map((task) => `<li>${task}</li>`).join('\n')}
+            </ul>
+            ${tags.map((tag) => `<div class="tag">${tag}</div>`).join('\n')}
+          </div>
+        </div>`;
+  }
+  return (
+    `<b>Indastrial</b>
+  <br />
+  <br />` +
+    cv.experiences.industrial
+      .map(({ title, companyName, icon, date, description, tasks, tags }) =>
+        getIndustrial(title, companyName, icon, date, description, tasks, tags)
+      )
+      .join('\n')
+  );
+}
+function getAcademics() {
+  function getAcademic(title, icon, date, description) {
+    return `<div class="card">
+        <img
+          class="industrial-card-icon"
+          src="${icon}"
+          alt="Farazbal"
+        />
+        <div>
+          <span class="card-title">
+            ${title}
+          </span>
+          <br />
+          <b>${date}</b>
+          <div>
+            ${description}
+          </div>
+        </div>
+      </div>`;
+  }
+
+  return (
+    `<b>Academic</b>
+  <br />
+  <br />` +
+    cv.experiences.academic
+      .map(({ title, icon, date, description }) =>
+        getAcademic(title, icon, date, description)
+      )
+      .join('\n')
+  );
+}
+
+function getCertificates() {
+  function getCertificate(title, icon, date, description, link) {
+    return `<div class="card">
+      <img
+        class="industrial-card-icon"
+        src="${icon}"
+        alt=""
+      />
+      <div>
+        <span class="card-title">
+          ${title}
+        </span>
+        <br />
+        <b>${date}</b>
+        <div>${description}</div>
+        <div>${link}</div>
+      </div>
+    </div>`;
+  }
+
+  return cv.certificates
+    .map(({ title, icon, date, description, link }) =>
+      getCertificate(title, icon, date, description, link)
+    )
+    .join('\n');
+}
+
+function getLanguages() {
+  function getLanguage(title, amount) {
+    return `<div class="lang">
+    ${title}
+    <div class="progress">
+      <div class="progress-val" style="width: ${amount}%"></div>
+    </div>
+  </div>`;
+  }
+
+  return cv.languages
+    .map(({ title, amount }) => getLanguage(title, amount))
+    .join('\n');
+}
+
+function getOpensourceProjects() {
+  function getOpensourceProject(title, icon, date, description, tags, link) {
+    return `<div class="card">
+            <img
+              class="industrial-card-icon"
+              src="${icon}"
+              alt=""
+            />
+            <div>
+              <span class="card-title">${title}</span>
+              <br />
+              <b>${date}</b>
+              <div>
+              ${description}
+              </div>
+              <div>${link}</div>
+              ${tags.map((tag) => `<div class="tag">${tag}</div>`).join('\n')}
+            </div>
+          </div>`;
+  }
+  return cv.opensourceProjects
+    .map(({ title, icon, date, description, tags, link }) =>
+      getOpensourceProject(title, icon, date, description, tags, link)
+    )
+    .join('\n');
+}
+
+function getSampleWorks() {
+  function getSampleWork(title, src, tags) {
+    return `<div class="card samplework-card" style="padding: 0">
+      <a href="${src}" target="_blank">
+      <img
+        class="samplework-screenshot"
+        src="${src}"
+        alt="${title}"
+      />
+      </a>
+      <br />
+      <div style="text-align: center; font-weight: bold; font-size: 18px">
+        ${title}
+      </div>
+      <div style="padding: 10px">
+        ${tags.map((tag) => `<div class="tag">${tag}</div>`).join('\n')}
+      </div>
+    </div>`;
+  }
+  return cv.sampleworks
+    .map(({ title, src, tags }) => getSampleWork(title, src, tags))
+    .join('\n');
+}
+
+function renderData() {
+  $(`#name`).html(cv.name);
+  $(`#summary`).html(cv.summary);
+  $(`#contact-info`).html(getContacts());
+  $(`#skills`).html(getSkills());
+  $(`#industrial-experiences`).html(getIndastrials());
+  $(`#academic-experiences`).html(getAcademics());
+  $(`#certificates`).html(getCertificates());
+  $(`#languages`).html(getLanguages());
+  $(`#opensource-projects`).html(getOpensourceProjects());
+  $(`#sampleworks-screenshots`).html(getSampleWorks());
+}
+
+$(document).ready(function () {
+  renderSeparators();
+  renderData();
+});
